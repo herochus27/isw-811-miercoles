2e3ccbafacd1a64f71e47302391cfd30959a5360
Herochus27 <orla_ndo21@hotmail.com>
2017-07-19 19:44:30 -0600

    count created

---
+++ clicks/migrations/20170720010127-contador.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.createTable('contadores',
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+       valor: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+    return queryInterface.dropTable('contadores');
+  }
+};
---
+++ clicks/routes/contadores.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+
+
+var criteria = function(req) {
+  return { where: {id: req.params.id} };
+}
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.format({
+    json: function () {
+      models.contadores.findAll().then(contadores => {
+        res.json({contadores: contadores});
+      });
+    },
+    html: function () {
+      models.contadores.findAll().then(contadores => {
+        res.render('index', { contadores: contadores });
+      });
+    }
+  });
+});
+
+router.post('/', function(req, res, next) {
+  var contador = models.contadores.create(req.body);
+  res.format({
+    json: function () {
+      contador.then(contador => {
+        res.json(contador);
+      });
+    },
+    html: function () {
+        contador.then(contador => {
+          res.redirect('/');
+      });
+    }
+  })
+});
+
+router.delete('/:id', function(req, res, next) {
+  res.format({
+    json: function() {
+      models.contadores.destroy(criteria(req)).then(() => {
+        res.json({status: 'ok'});
+      });
+    },
+    html: function () {
+      models.contadores.destroy(criteria(req)).then(() => {
+        res.redirect('/api/v1/products');
+      });
+    }
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  res.format({
+    json: function () {
+      models.contadores.update(req.body, criteria(req)).then(contador => {
+        res.json(contador);
+      });
+    },
+    html: function () {
+      models.contadores.update(req.body, criteria(req)).then(contador => {
+        res.redirect('/api/v1/products');
+      });
+    }
+  });
+});
+
+
+module.exports = router;
