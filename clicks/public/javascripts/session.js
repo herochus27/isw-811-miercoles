b80c8c07502d6287b18cbd91d06d0a0b7be22153
Keisersv17 <keisersvsv@mail.com>
2017-07-25 22:48:27 -0600

    Finish session

--- clicks/public/javascripts/session.js
+++ clicks/public/javascripts/session.js
@@ -1,15 +1,42 @@
 var Session = {
     getData: function(){
-      return $.getJSON('/session');
+      return $.getJSON('/sesion');
     },
     insert: function (data){
-      return $.post('/session', data);
+      return $.post('/sesion', data);
     }
 };
+
+$(document).ready(function(){
+  $("#clickSession").on("click", function () {
+    var count = $(this).val();
+    count++;
+    $(this).val(count);
+    $("#lblCount").text(count);
+
+    var getCount= {count:count};
+    Session.insert(getCount).done(function(json){
+      saveSession();
+    });
+  });
+
+  $("#resetSession").on("click",function() {
+    $("#lblCount").text(0);
+    $("#clickSession").val(0);
+    var getCount= {count: 0};
+    Session.insert(getCount).done(function(json){
+      saveSession();
+    });
+  });
+
+    saveSession();
+});
+
 function saveSession(){
-  session.getData().done(function(json){
+  Session.getData().done(function(json){
     console.log(json);
     $('#clickSession').val(json);
     $('#lblCount').text(json);
   });
 }
+
--- clicks/routes/sessions.js
+++ clicks/routes/sessions.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 
 var criteria = function(req) {
-  return { where: {id: req.params.id} };
+	return { where: {id: req.params.id} };
 }
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-res.json({count: 1 });
+	res.send(req.session.count);
+});
+
+router.post('/', function(req, res, next) {
+	req.session.count = req.body.count;
+	res.send(req.session.count);
 });
 
 module.exports = router;
